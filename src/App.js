5e719b991c0fffa9313f9403f3a42e30afbd224b
rizwan hasan <rizwanhasan897@gmail.com>
2023-11-27 23:46:40 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import AdminDashboard from './admin-ui/pages/AdminDashboard';
-import Home from './customer-ui/pages/Home';
 
 function App() {
   return (
     <div>
       <h1>fds</h1>
-      <AdminDashboard />
-      <Home />
+
+
     </div>
   );
 }
