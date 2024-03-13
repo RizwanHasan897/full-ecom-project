8c6a2ff1ceff411bc2fd159d8f10195422676708
rizwan hasan <rizwanhasan897@gmail.com>
2024-03-13 03:39:48 +0000

    Update Home.js

--- src/customer-ui/pages/Home.js
+++ src/customer-ui/pages/Home.js
@@ -3,19 +3,26 @@ import HomeHeader from '../components/HomeHeader';
 import Items from '../components/Items';
 
 function Home() {
+
     const [shoppingCart, setShoppingCart] = useState([]);
+    const [itemsData, setItemsData] = useState([]);
 
     useEffect(() => {
         const storedCart = localStorage.getItem('shoppingCart');
         if (storedCart) {
             setShoppingCart(JSON.parse(storedCart));
         }
+
+        const storedItemsData = localStorage.getItem('tableData');
+        if (storedItemsData) {
+            setItemsData(JSON.parse(storedItemsData));
+        }
     }, []);
 
     return (
         <div className='customer-home'>
             <HomeHeader shoppingCart={shoppingCart} />
-            {1 ? <Items setShoppingCart={setShoppingCart} /> : <div></div>}
+            {itemsData.length > 0 ? <Items setShoppingCart={setShoppingCart} /> : <div></div>}
         </div>
     );
 }
